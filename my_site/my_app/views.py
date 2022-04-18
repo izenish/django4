b665282ce424edc0dda0a8314633135fbdedc915
Zenish Prajapati <izenish@Zenishs-MacBook-Air.local>
2022-04-18 08:43:54 +0545

    working with templates and {{JINJA}}

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def example_view(request):
     #my_app/templates/my_app/example.html
     return render(request,'my_app/example.html')
+
+def variable_view(request):
+    my_var={
+        'first_name':'Jenish',
+        'last_name':'Prajapati',
+        'some_list':[1,22,3],
+        'some_dict':{'inside_key':'inside_value'}
+    }
+    return render(request,'my_app/variable.html',context=my_var)
